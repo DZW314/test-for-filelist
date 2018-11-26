8b73db588ca7de9bf85630981d1bf39d474389de
DZW <wei.zhao314@qq.com>
2018-11-26 18:26:11 +0800

    update: Add files' name and crc value into a excel.

--- crc32.py
+++ crc32.py
@@ -1,3 +1,5 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook
 from zlib import crc32
 import sys
 import glob
@@ -21,10 +23,10 @@ logging.basicConfig(filename='info.log', level=logging.DEBUG, format=LOG_FORMAT)
 #search all files and store in an array
 logging.info("Start search all files in Packages.")
 listFile = []
-listuxz = glob.glob("./*.uxz")
-listbin = glob.glob("./*.bin")
-listtgz = glob.glob("./*.tgz")
-listexe = glob.glob("./*.exe")
+listuxz = glob.glob("./*/*.uxz")
+listbin = glob.glob("./*/*.bin")
+listtgz = glob.glob("./*/*.tgz")
+listexe = glob.glob("./*/*.exe")
 listFile.extend(listuxz)
 listFile.extend(listbin)
 listFile.extend(listtgz)
@@ -33,14 +35,30 @@ logging.info("Finish searching.")
 #list sort
 logging.info("Sorting the list...")
 sortFile = sorted(listFile)
-for n in sortFile:
-    logging.info(n)
-#for n in list:
-#    print('{:s} {:8} {:x}'.format( n,' crc32: ', getCrc32(n)))
 
+#Create sheet for excel
+wb = Workbook()
+ws = wb.active
+ws.title = "platform"
+ws1 = wb.create_sheet("win2019")
+ws2 = wb.create_sheet("win2016")
+ws3 = wb.create_sheet("rhel7")
+ws4 = wb.create_sheet("suse12")
+
+#Check all packages by crc32
+row = 1
+for n in sortFile:
+    crc = format(getCrc32(n), 'x')
+    print('{:s} {:8} {:s}'.format( n,' crc32: ', crc))
+    logging.info('{:s} {:8} {:s}'.format( n,' crc32: ', crc))
+    ws.cell(column=1, row=row, value=n)
+    ws.cell(column=2, row=row, value=crc)
+    row = row + 1
+wb.save("sample.xlsx")
+logging.info("Mission Completed!")
 # result =  'crc32.txt'
 # f = open ('./' + result,'w')
-# for n in list:
+# for n in sortFile:
 #     str = n;
 #     crc = format(getCrc32(n), 'x')
 #     print('{:s} {:8} {:x}'.format( n,' crc32: ', getCrc32(n)))
