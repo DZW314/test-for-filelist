06fa236a7523f3dcf58eef52218683865266e623
DZW <wei.zhao314@qq.com>
2018-12-20 15:02:25 +0800

    Update fileList.bat

--- fileList.bat
+++ fileList.bat
@@ -1,12 +1,54 @@
-#del 0supslist.txt
-#for %%i in (*.exe) do @echo %%i >> 0supslist.txt
-#for %%i in (*.tgz) do @echo %%i >> 0supslist.txt
-#for %%i in (*.bin) do @echo %%i >> 0supslist.txt
-#for %%i in (*.uxz) do @echo %%i >> 0supslist.txt
 
+del anyos.txt
+for %%i in (./c5sp05p-1.50_anyos/*.exe) do @echo %%~ni >> anyos.txt
+for %%i in (./c5sp05p-1.50_anyos/*.tgz) do @echo %%~ni >> anyos.txt
+for %%i in (./c5sp05p-1.50_anyos/*.bin) do @echo %%~ni >> anyos.txt
+for %%i in (./c5sp05p-1.50_anyos/*.uxz) do @echo %%~ni >> anyos.txt
 
-del 0supslist.txt
-for %%i in (*.exe) do @echo %%~ni >> 0supslist.txt
-for %%i in (*.tgz) do @echo %%~ni >> 0supslist.txt
-for %%i in (*.bin) do @echo %%~ni >> 0supslist.txt
-for %%i in (*.uxz) do @echo %%~ni >> 0supslist.txt
+del platform.txt
+for %%i in (./c5sp05p-1.50_platform/*.exe) do @echo %%~ni >> platform.txt
+for %%i in (./c5sp05p-1.50_platform/*.tgz) do @echo %%~ni >> platform.txt
+for %%i in (./c5sp05p-1.50_platform/*.bin) do @echo %%~ni >> platform.txt
+for %%i in (./c5sp05p-1.50_platform/*.uxz) do @echo %%~ni >> platform.txt
+
+del win2012r2.txt
+for %%i in (./c5sp05p-1.50_win2012r2/*.exe) do @echo %%~ni >> win2012r2.txt
+for %%i in (./c5sp05p-1.50_win2012r2/*.tgz) do @echo %%~ni >> win2012r2.txt
+for %%i in (./c5sp05p-1.50_win2012r2/*.bin) do @echo %%~ni >> win2012r2.txt
+for %%i in (./c5sp05p-1.50_win2012r2/*.uxz) do @echo %%~ni >> win2012r2.txt
+
+del win2016.txt
+for %%i in (./c5sp05p-1.50_win2016/*.exe) do @echo %%~ni >> win2016.txt
+for %%i in (./c5sp05p-1.50_win2016/*.tgz) do @echo %%~ni >> win2016.txt
+for %%i in (./c5sp05p-1.50_win2016/*.bin) do @echo %%~ni >> win2016.txt
+for %%i in (./c5sp05p-1.50_win2016/*.uxz) do @echo %%~ni >> win2016.txt
+
+del win2019.txt
+for %%i in (./c5sp05p-1.50_win2019/*.exe) do @echo %%~ni >> win2019.txt
+for %%i in (./c5sp05p-1.50_win2019/*.tgz) do @echo %%~ni >> win2019.txt
+for %%i in (./c5sp05p-1.50_win2019/*.bin) do @echo %%~ni >> win2019.txt
+for %%i in (./c5sp05p-1.50_win2019/*.uxz) do @echo %%~ni >> win2019.txt
+
+del rhel6.txt
+for %%i in (./c5sp07p-1.50_rhel6/*.exe) do @echo %%~ni >> rhel6.txt
+for %%i in (./c5sp07p-1.50_rhel6/*.tgz) do @echo %%~ni >> rhel6.txt
+for %%i in (./c5sp07p-1.50_rhel6/*.bin) do @echo %%~ni >> rhel6.txt
+for %%i in (./c5sp07p-1.50_rhel6/*.uxz) do @echo %%~ni >> rhel6.txt
+
+del rhel7.txt
+for %%i in (./c5sp08p-1.50_rhel7/*.exe) do @echo %%~ni >> rhel7.txt
+for %%i in (./c5sp08p-1.50_rhel7/*.tgz) do @echo %%~ni >> rhel7.txt
+for %%i in (./c5sp08p-1.50_rhel7/*.bin) do @echo %%~ni >> rhel7.txt
+for %%i in (./c5sp08p-1.50_rhel7/*.uxz) do @echo %%~ni >> rhel7.txt
+
+del sles12.txt
+for %%i in (./c5sp08p-1.50_sles12/*.exe) do @echo %%~ni >> sles12.txt
+for %%i in (./c5sp08p-1.50_sles12/*.tgz) do @echo %%~ni >> sles12.txt
+for %%i in (./c5sp08p-1.50_sles12/*.bin) do @echo %%~ni >> sles12.txt
+for %%i in (./c5sp08p-1.50_sles12/*.uxz) do @echo %%~ni >> sles12.txt
+
+del sles15.txt
+for %%i in (./c5sp05p-1.50_sles15/*.exe) do @echo %%~ni >> sles15.txt
+for %%i in (./c5sp05p-1.50_sles15/*.tgz) do @echo %%~ni >> sles15.txt
+for %%i in (./c5sp05p-1.50_sles15/*.bin) do @echo %%~ni >> sles15.txt
+for %%i in (./c5sp05p-1.50_sles15/*.uxz) do @echo %%~ni >> sles15.txt
